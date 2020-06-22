89eea20cb5c8ba43cdee75d91cbe746aa0fa0152
kenlsm <kenlsm@outlook.com>
2020-06-23 02:33:04 +0800

    update styles, remove typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,44 +1,5 @@
 import Typography from "typography";
-import Noriega from "typography-theme-noriega";
-
-Noriega.overrideThemeStyles = () => {
-  return {
-    "a:-webkit-any-link": {
-      color: "honeydew",
-      textDecorationColor: "#cccbff",
-    },
-    html: {
-      backgroundColor: "darkslateblue",
-    },
-    body: {
-      color: "navajowhite",
-    },
-    h1: {
-      color: "honeydew",
-    },
-    h2: {
-      color: "honeydew",
-    },
-    h3: {
-      color: "honeydew",
-    },
-    li: {
-      listStyleType: "hiragana-iroha",
-    },
-    blockquote: {
-      backgroundColor: "#f1f1f1",
-      color: "#1e2e54",
-      margin: "12px",
-      padding: "6px",
-      borderLeftColor: "green",
-      borderLeftWidth: "6px",
-      borderLeftStyle: "solid",
-      fontStyle: "italic",
-    },
-  };
-};
-
-const typography = new Typography(Noriega);
+const typography = new Typography();
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
