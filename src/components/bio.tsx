03345bd65c47cb27928bd9a2654a253503776bf4
kenlsm <kenlsm@outlook.com>
2023-12-22 01:10:38 +0800

    chore: fix bio

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -5,18 +5,23 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image";
 
-const Bio = () => {
+import { rhythm } from "../utils/typography";
+const Bio = ({ short }) => {
   const data = useStaticQuery(graphql`
     query BioQuery {
+      avatar: file(absolutePath: { regex: "/profile-pic.*/" }) {
+        childImageSharp {
+          gatsbyImageData(width: 100, layout: FIXED)
+        }
+      }
       site {
         siteMetadata {
           author {
             name
-            summary
           }
           social {
             twitter
@@ -24,12 +29,49 @@ const Bio = () => {
         }
       }
     }
-  `)
+  `);
 
-  // Set these values by editing "siteMetadata" in gatsby-config.js
-  const author = data.site.siteMetadata?.author
-  const social = data.site.siteMetadata?.social
-
+  const { author, social } = data.site.siteMetadata;
+  console.log({ author, social });
+  const image = getImage(data.avatar.childImageSharp);
+  return (
+    <div
+      style={{
+        display: `flex`,
+        marginBottom: rhythm(2.5),
+      }}
+    >
+      {/* {image && (
+        <GatsbyImage
+          image={image}
+          alt={author}
+          style={{
+            flexShrink: 0,
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            minWidth: rhythm(5 / 2),
+            borderRadius: `100%`,
+          }}
+          imgStyle={{
+            borderRadius: `50%`,
+          }}
+        />
+      )} */}
+      <div>
+        <p style={{ marginBottom: 0 }}>
+          I'm{" "}
+          <a href={`https://twitter.com/${social.twitter}`}>{author.name}</a> 🇸🇬
+        </p>
+        <p>
+          {short
+            ? `Thinking and trying to build useful things.`
+            : `Penning down thoughts, and ideas to share. During my commute, my
+          brain digests through many thoughts of others. And through them, I pen
+          down thoughts and ideas of yestermorning.`}
+        </p>
+      </div>
+    </div>
+  );
   return (
     <div className="bio">
       <StaticImage
@@ -52,7 +94,7 @@ const Bio = () => {
         </p>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Bio
+export default Bio;
