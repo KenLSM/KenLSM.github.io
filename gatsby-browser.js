b5a48e407449b1e0fd1fc01c993de61aa7a21d09
kenlsm <kenlsm@outlook.com>
2023-12-22 17:39:04 +0800

    chore: sync css with old

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,11 @@
 // custom typefaces
 import "@fontsource-variable/montserrat"
-import "@fontsource/merriweather"
-// import "prismjs/plugins/line-number/prism-line-numbers.css"
-import "prismjs/themes/prism-tomorrow.css"
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
-
+import "@fontsource/merriweather" 
+import "prismjs/themes/prism-tomorrow.css" 
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
 // normalize CSS across browsers
-import "./src/normalize.css"
+import "./src/styles/normalize.css"
 
 // custom CSS styles
-import "./src/style.css"
-
+import "./src/styles/style.css"
+import "./src/styles/global.css"
