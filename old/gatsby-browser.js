7a6b2c75ab30aa1d7c027930d6cc6aaa8d1898b5
kenlsm <kenlsm@outlook.com>
2023-12-22 17:45:09 +0800

    chore: remove old folder

--- old/gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-// custom typefaces
-import "typeface-montserrat";
-import "typeface-merriweather";
-require("prismjs/themes/prism-tomorrow.css");
-require("./src/styles/global.css");
-require("prismjs/plugins/line-numbers/prism-line-numbers.css");
